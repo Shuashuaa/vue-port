da11600eed824cc427c27e4ca634f7078b2f3513
Shuashuaa <96514740+Shuashuaa@users.noreply.github.com>
2023-12-20 21:50:12 +0800

    122023_STABLE_INFO

---
+++ src/components/main/InfoComp.vue
@@ -0,0 +1,33 @@
+<template>
+	<div id="goToInfo" class="justify-center d-flex section" style="background-color: #f5f5f5; min-height:500px; padding-top: 100px; padding-bottom: 100px; position: relative">
+
+		<!-- pc -->
+
+		<div id="goToInfo" class="d-none d-lg-block d-xl-block section" style="width: 60%;">
+			<!-- contents -->
+		</div>
+
+		<!-- mobile -->
+
+		<div id="info-canvas-mobile" class="d-block d-sm-block d-md-block d-lg-none">
+			<!-- contents -->
+		</div>
+
+	</div>
+</template>
+
+<style>
+	/* .shadow-in{
+		background-color: #343a40;
+		-webkit-background-clip: text;
+		-moz-background-clip: text;
+		background-clip: text;
+		color: transparent;
+		text-shadow: rgba(210, 210, 210, 0.5) 0px 3px 3px;
+	}
+
+	.parentBg {
+		background:#f5f5f578 !important;
+	} */
+
+</style>
