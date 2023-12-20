7714ab044a454157691d758d609193c9667d390d
Shuashuaa <96514740+Shuashuaa@users.noreply.github.com>
2023-12-20 21:47:43 +0800

    122023_STABLE_HOME

---
+++ src/components/main/HomeComp.vue
@@ -0,0 +1,34 @@
+<template>
+	<v-card
+		:height="scHeight"
+		id="goToHome"
+		class="justify-center d-flex bg section"
+		style="background-color: #f5f5f5"
+		elevation="0"
+	>
+		<!-- contents -->
+	</v-card>
+</template>
+
+<script>
+// import { ref, onMounted } from "vue";=
+export default {
+	setup() {
+
+		// this will fix the page's height depends on the users screen height
+		const scHeight = innerHeight;
+
+		onMounted(() => {
+			console.log(innerHeight);
+		});
+
+		return { scHeight };
+	},
+};
+</script>
+
+<style>
+
+/*  */
+
+</style>
