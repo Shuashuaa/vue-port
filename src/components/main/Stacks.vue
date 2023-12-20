fbda64f402cce153b317ddcf68f6c77dbbb0c823
Shuashuaa <96514740+Shuashuaa@users.noreply.github.com>
2023-12-20 21:48:59 +0800

    122023_STABLE_STACKS

---
+++ src/components/main/Stacks.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="goToStacks" class="justify-center d-flex" style="background-color: #f5f5f5; min-height:800px; padding-top: 100px; padding-bottom: 100px; position: relative;">
+
+    <div id="stack-canvas"> 
+        <!-- contents -->
+    </div>
+
+  </div>
+</template>
+
+<script>
+// import { ref } from 'vue';
+export default {
+    setup() {
+        //
+
+        return { 
+            //
+        }
+    }
+}
+</script>
