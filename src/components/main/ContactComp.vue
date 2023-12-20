cceb135b0607af8f52eb4840f94886775a92da46
Shuashuaa <96514740+Shuashuaa@users.noreply.github.com>
2023-12-20 21:47:27 +0800

    122023_STABLE_CONTACT

---
+++ src/components/main/ContactComp.vue
@@ -0,0 +1,36 @@
+<template>
+	<div id="goToContact" class="justify-center d-flex" style="background-color: #343a40; padding-top: 50px; padding-bottom: 50px;">
+
+		<!-- pc -->
+
+		<div id="canvas" class="d-none d-md-flex d-lg-flex d-xl-flex">
+			<!-- contents -->
+			
+		</div>
+		
+		<!-- mobile -->
+
+		<div id="canvas" class="d-block d-sm-block d-md-none">
+			<!-- contents -->
+
+		</div>
+		
+	</div>
+</template>
+
+<script>
+// import { ref } from 'vue';
+export default {
+	setup() {
+		// 
+
+		return { 
+			// 
+		}
+	}
+}
+</script>
+
+<style>
+	/*  */
+</style>
