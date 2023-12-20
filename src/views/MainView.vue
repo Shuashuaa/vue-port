12c7e9c59655da8e9dc0cff4f31a7bed8cf1a486
Shuashuaa <96514740+Shuashuaa@users.noreply.github.com>
2023-12-20 21:49:28 +0800

    122023_STABLE_MAINVIEW

---
+++ src/views/MainView.vue
@@ -0,0 +1,34 @@
+<template>
+    <HomeComp id="#" class="section" />
+    <InfoComp id="info" class="section" />
+    <Stacks id="stacks" class="section" />
+    <ProductsComp id="projects" class="section" />
+    <ContactComp />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import HomeComp from '../components/main/HomeComp.vue';
+import InfoComp from '../components/main/InfoComp.vue';
+import Stacks from '../components/main/Stacks.vue';
+import ProductsComp from '../components/main/ProductsComp.vue';
+import ContactComp from '../components/main/ContactComp.vue';
+
+export default defineComponent({
+    components: {
+        HomeComp,
+        InfoComp,
+        Stacks,
+        ProductsComp,
+        ContactComp
+    },
+});
+</script>
+
+<style>
+    html {
+        scroll-behavior: smooth;
+    }
+</style>
