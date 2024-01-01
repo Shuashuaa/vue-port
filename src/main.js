de6c11e422840a1b8e06ba2e6b1904fd9bf72cc4
Shuashuaa <96514740+Shuashuaa@users.noreply.github.com>
2024-01-01 10:50:57 +0800

    TITLE_MANIPULATION_DYNAMIC

--- src/main.js
+++ src/main.js
@@ -9,6 +9,15 @@ import VueKinesis from "vue-kinesis";
 
 loadFonts()
 
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  } else {
+    document.title = '404';
+  }
+  next();
+});
+
 createApp(App)
   .use(router)
   .use(vuetify) //mdi
