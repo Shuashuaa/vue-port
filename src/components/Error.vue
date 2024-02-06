a071a6e3bb7d3ebbfc542fae2cb83caf11c3fb24
Shuashuaa <jovijoshuatania027@gmail.com>
2024-02-06 18:09:44 +0800

    020624_ERROR

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -5,7 +5,7 @@
                 <v-container  
                 fluid
                 class="loginComponent"
-                style="padding-top: 300px; position: relative;"
+                style="padding-top: 27vh; position: relatives;"
                 >
                     <h1 class="gomenMobile d-block d-sm-none bold-pop">`Gomen'nasai'</h1>
 
@@ -17,7 +17,7 @@
                     </h3>
                     <p class="mb-6">The page you are looking for might have been removed, had it's name changed or is temporarily unavailable.</p> 
                     
-                    <v-btn large class="med-pop" href="/home/" rounded outlined style="color: white; background-color: #343a40;">
+                    <v-btn large class="med-pop" href="/##" rounded outlined style="color: white; background-color: #343a40;">
                         <v-icon>mdi-arrow-left</v-icon>
                         Go to homepage
                     </v-btn>
